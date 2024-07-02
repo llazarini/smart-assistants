0d371c23086bef523f216fc096aa408909a293a2
Leonardo Lazarini <llazarini2@gmail.com>
2024-07-02 18:18:47 +0100

    Few adjustments on imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export * from './assistants/assistant.js';
-export * from './assistants/logger.js';
-export * from './assistants/tool.js';
 export * from './assistants/chat_history.js';
+export * from './assistants/logger.js';
+export * from './assistants/run.js';
+export * from './assistants/tool.js';
 
 export * from './language_models/language_model.js';
 export * from './language_models/openai.js';
